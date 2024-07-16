8d1d89d40f106f988cfeac1c3a1b90887dd50d7c
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-07-16 18:45:56 +0000

    [Finish] 4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,22 @@ class User < ApplicationRecord
   #PostImageモデルの関連付け（1:Nの1側にあたるモデルにhas_manyを記載）
   has_many :post_images, dependent: :destroy
   #dependent: :destroy → 1:Nの1側が削除されたとき、N側を全て削除する
+
+  #profile_imageという名前で、ActiveStorageでプロフィール画像を保存できるよう設定
+  has_one_attached :profile_image
+
+  #画像が投稿されない場合のエラー回避
+  #アクションと違い、特定の処理を名前で呼び出す
+  def get_profile_image
+    unless profile_image.attached?
+      #画像が設定されない場合はapp/assets/imagesに格納されているsample-author1.jpgを
+          #デフォルト画像としてActiveStorageに格納し、格納した画像を表示する
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    #画像サイズ変更（引数width, heightを設定）：画像をwidth×heightのサイズに変換する
+    profile_image.variant(resize_to_limit: [width, height]).processed
+    # ↑このメソッドを実行する際get_profile_image(100, 100)のように引数を設定すると100x100の画像にリサイズされる
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   root to: "homes#top"    #ルートパスで設定
 
   #resourcesメソッド：ルーティングを一括して自動生成
+  #post_imagesモデルのルーティング
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   #onlyオプションを使うと生成するルーティングを限定できる
 
+  #usersモデルのルーティング
+  resources :users, only: [:show, :edit]
+
   get 'homes/about', to: 'homes#about', as: 'about'
+
 end
